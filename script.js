8b4bc6232b3e139c5b45957dc5e60f93d74dd003
Anna <ryavkina@mail.ru>
2020-07-29 16:46:18 +0500

    task 2

--- script.js
+++ script.js
@@ -9,13 +9,15 @@ const personalMovieDB = {
     genres: [],
     private: false
 };
-
-let q1 = prompt('Один из последних просмотренных файлов?', ''),
-    q2 = prompt('На сколько оцените его?', ''),
-    q12 = prompt('Один из последних просмотренных файлов?', ''),    
-    q22 = prompt('На сколько оцените его?', '');
-
-personalMovieDB.movies[q1]=q2;
-personalMovieDB.movies[q12]=q22;
+let i = 1;
+while (i <= 2) {
+    let q1 = prompt('Один из последних просмотренных файлов?', ''),
+        q2 = prompt('На сколько оцените его?', '');
+    if (q1 == null || q2 == null || q1 == '' || q2 == '' || q1.length > 50) {
+        continue;
+    }
+    personalMovieDB.movies[q1] = q2;
+    i++;
+}
 
 console.log(personalMovieDB);
