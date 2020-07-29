85ffe8541bcb54267b85aed89cf3881e09a64693
Anna <ryavkina@mail.ru>
2020-07-29 17:44:38 +0500

    task-function

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 "use strict";
 
-let numberOfFilms = prompt("Сколько фильмов вы уже помотрели?", "");
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt("Сколько фильмов вы уже помотрели?", "");
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt("Сколько фильмов вы уже помотрели?", "");
+    }
+}
+start();
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -9,15 +18,37 @@ const personalMovieDB = {
     genres: [],
     private: false
 };
-let i = 1;
-while (i <= 2) {
-    let q1 = prompt('Один из последних просмотренных файлов?', ''),
-        q2 = prompt('На сколько оцените его?', '');
-    if (q1 == null || q2 == null || q1 == '' || q2 == '' || q1.length > 50) {
-        continue;
+
+function rememberMyFilms() {
+    let i = 1;
+    while (i <= 2) {
+        let q1 = prompt('Один из последних просмотренных файлов?', ''),
+            q2 = prompt('На сколько оцените его?', '');
+        if (q1 == null || q2 == null || q1 == '' || q2 == '' || q1.length > 50) {
+            continue;
+        }
+        personalMovieDB.movies[q1] = q2;
+        i++;
     }
-    personalMovieDB.movies[q1] = q2;
-    i++;
+}
+function detectPeronallevel(){
+    if(personalMovieDB.count<10){
+        console.log("Мало фильмов");
+    }else if(personalMovieDB.count>=10 && personalMovieDB.count<=30){
+        console.log("Классический зритель");
+    }else if(personalMovieDB.count>30){
+        console.log("Киноман");
+    }
 }
 
-console.log(personalMovieDB);
+function showMyDB(hidden){
+    if(!hidden){
+        console.log(personalMovieDB);
+    }
+}
+
+function writeYouGengres(){
+    for(let i=1;i<=3;i++){
+        personalMovieDB.genres[i-1] = prompt(`Ваш любимый жанр под номером ${i}`,'');
+    }
+}
